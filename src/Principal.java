327f03085281121ff23ca1b209762774fa4c43ea
LucianeC <costaluciane087@gmail.com>
2023-08-21 16:49:36 -0300

    Atualização Pt2

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,20 @@
+import itau.magicscreen.modelo.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
-        filme1.nome = "Poderoso Chefão";
-        filme1.anoDeLancamento =1970;
-        filme1.duracaoEmMinutos = 180;
-        filme1.incluidoNoPlano = false;
+        filme1.setNome("Poderoso Chefão");
+        filme1.setAnoDeLancamento(1970);
+        filme1.setDuracaoEmMinutos(180);
+        filme1.setIncluidoNoPlano(false);
+
 
         filme1.exibeFichaTecnica();
         filme1.avalia(9);
         filme1.avalia(4);
         filme1.avalia(6);
         filme1.avalia(10);
-        System.out.println(filme1.somaDasAvaliacoes);
-        System.out.println(filme1.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
         System.out.println(filme1.pegaMedia());
 
     }
